0c220229fd55bf3f6315b5cfff67761c1ec7a42f
Gianfranco Dutka <gdutk007@gmail.com>
2021-02-07 04:19:27 -0500

    Almost done with programs. Now just need to finish delete and then add multithreading to finish.

--- Advanced-Systems-Programming/Assignment2/locationUpdater.cpp
+++ Advanced-Systems-Programming/Assignment2/locationUpdater.cpp
@@ -12,6 +12,7 @@ typedef enum {
 	DATE,
 	TIME,
 	LOCATION,
+    DONE,
 	NONE
 } parse_state_t;
 
@@ -25,7 +26,7 @@ typedef struct {
 
 static int BufferLength;
 
-map<string, Event_t> CalendarFilter;
+multimap<string, Event_t> CalendarFilter;
 
 vector<string> ValidEmails;
 
@@ -38,21 +39,47 @@ bool ParseValidEmails();
 bool CheckEmailString(string line);
 
 bool CheckAction(string action);
+
 bool CheckTitle(string title);
+
 bool CheckDate(string date);
+
 bool CheckTime(string time);
+
 bool CheckLocation(string location);
 
-int main(int argc, char *argv[]){
+bool processCalendarThread();
 
+bool processCalendar(string email);
+
+bool CreateEvent(Event_t Event);
+
+bool EditEvent(Event_t Event);
+
+bool DeleteEvent(Event_t Event);
+
+Event_t searchEarliestEventByDate(string date);
+
+bool CompareTime(string time1, string time2);
+
+bool CompareEvents(Event_t event1, Event_t event2);
+
+multimap<string,Event_t>::iterator SearchEventByTitle(string title);
+
+int main(int argc, char *argv[]){
     bool validArg = parseArguments(argc,argv);
     if(!validArg && BufferLength != 0){
         cout << "Please Enter Valid Arguments! Program Terminating\n";
         exit(0);
     }
-    Event_Buffer.resize(BufferLength);
 
     ParseValidEmails();
+    for(auto email : ValidEmails){
+        cout << email << endl;
+    }
+    cout << endl;
+
+    processCalendarThread();
 
     return 0;
 }
@@ -65,7 +92,6 @@ bool parseArguments(int argc, char *argv[]){
     }
     string bufferLen(argv[1]);
     const long len = strtol(bufferLen.c_str(),nullptr,10); 
-
     if( len < 0){
         cout << "please enter a valid number.\n";
         return false;
@@ -79,7 +105,10 @@ bool ParseValidEmails(){
         if(strlen(line.c_str()) > 9){
             size_t pos = line.find("Subject: ");
             if(pos != string::npos){
-                CheckEmailString(line.substr (pos+strlen("Subject: ")));
+                string email = line.substr(pos+strlen("Subject: "));
+                if(CheckEmailString(email)){
+                    ValidEmails.push_back(email);
+                }
             }
         }
     }
@@ -87,49 +116,204 @@ bool ParseValidEmails(){
 }
 
 bool CheckEmailString(string line){
-    cout << line << endl;
     istringstream is(line);
     string part;
     parse_state_t state = ACTION;
     while (getline(is, part, ',')){
-        cout << part << endl;
         switch (state)
         {
         case ACTION:
-            /* code */
+            state = (CheckAction(part)) ? TITLE : NONE;
         break;
         case TITLE:
-            /* code */
+            state = (CheckTitle(part)) ? DATE : NONE;
         break;
         case DATE:
-            /* code */
+            state = (CheckDate(part)) ? TIME : NONE;
         break;
          case TIME:
-            /* code */
+            state = (CheckTime(part)) ? LOCATION : NONE;
         break;
         case LOCATION:
-            /* code */
+            state = (CheckLocation(part)) ? DONE : NONE;
         break;       
         default:
+            return false;
             break;
         }
     }
-
-    return true;
+    return state == DONE;
 }
 
 bool CheckAction(string action){
-    return true;
+    string Create("C"), Edit("X"), Delete("D");
+    return (action.length()==1 && (action == Create || action == Edit || action == Delete));
 }
 bool CheckTitle(string title){
+    // can titles have numebers????
+    if( title.length() != 10 &&
+        title.find('0') != std::string::npos ||
+        title.find('1') != std::string::npos ||
+        title.find('2') != std::string::npos ||
+        title.find('3') != std::string::npos ||
+        title.find('4') != std::string::npos ||
+        title.find('5') != std::string::npos ||
+        title.find('6') != std::string::npos ||
+        title.find('7') != std::string::npos ||
+        title.find('8') != std::string::npos ||
+        title.find('9') != std::string::npos)
+    {
+        return false;
+    }
     return true;
 }
 bool CheckDate(string date){
+    if(date.length() != 10) return false;
+    istringstream is(date);
+    string part;
+    while(getline(is, part, '/')){
+        bool isNum = !part.empty() && std::find_if(part.begin(),part.end(), 
+                                     [](unsigned char c) { return !std::isdigit(c); }) == part.end();
+        if(!isNum) return false; // if there's no digit return false
+    }
     return true;
 }
 bool CheckTime(string time){
+    if(time.length() != 5) return false;
+    istringstream is(time);
+    string part;
+    while(getline(is, part, ':')){
+        bool isNum = !part.empty() && std::find_if(part.begin(),part.end(), 
+                                     [](unsigned char c) { return !std::isdigit(c); }) == part.end();
+        if(!isNum) return false; // if there's no digit return false
+    }
     return true;
 }
 bool CheckLocation(string location){
+    return location.length() == 10;
+}
+
+bool processCalendarThread(){
+    for(string email : ValidEmails){
+        processCalendar(email);    
+    }
     return true;
+}
+
+bool processCalendar(string email){
+    istringstream is(email);
+    string part;
+    Event_t Event;
+
+    getline(is, part, ',');
+    Event.Action = part;
+    getline(is, part, ',');
+    Event.Title = part;
+    getline(is, part, ',');
+    Event.Date = part;
+    getline(is, part, ',');
+    Event.Time = part;
+    getline(is, part, ',');
+    Event.Location = part;
+    
+    if( Event.Action == "C"){
+        CreateEvent(Event);   
+    }else if(Event.Action == "X"){
+        EditEvent(Event);
+    }else if(Event.Action == "D"){
+        DeleteEvent(Event);
+    }else{
+        // error we shouldn't be here
+    }
+    return true;
+}
+
+bool CreateEvent(Event_t Event){
+    if(CalendarFilter.find(Event.Date) == CalendarFilter.end()){
+        CalendarFilter.emplace(Event.Date,Event);
+        cout << Event.Date << " " << Event.Time << " " << Event.Location << endl;  
+    }
+    else{
+        CalendarFilter.emplace(Event.Date,Event);
+        Event_t earliestEvent = searchEarliestEventByDate(Event.Date);
+        if(  CompareEvents(earliestEvent,Event) ){
+            cout << Event.Date << " " << Event.Time << " " << Event.Location << endl;  
+        }
+    }
+    return true;
+}
+
+bool EditEvent(Event_t Event){
+    // need to work on edit event
+    auto iter = SearchEventByTitle(Event.Title);
+    
+    if(iter == CalendarFilter.end()) return false;
+    iter->second = Event;
+    Event_t earliestEvent = searchEarliestEventByDate(Event.Date);
+    
+    if( CompareEvents(iter->second,earliestEvent) ){
+        cout << iter->second.Date << " " << iter->second.Time << " " << iter->second.Location << endl; 
+    }
+    return true;
+}
+
+bool DeleteEvent(Event_t Event){
+    return true;
+}
+
+Event_t searchEarliestEventByDate(string date){
+    Event_t earliestEvent = CalendarFilter.find(date)->second;
+    for(auto iter = CalendarFilter.begin(); iter != CalendarFilter.end(); ++iter){
+        if( iter->second.Date == date ){
+            if(!CompareTime(earliestEvent.Time,iter->second.Time)){
+                earliestEvent = iter->second;
+            }
+        }
+    }
+    return earliestEvent;
+}
+
+bool CompareTime(string time1, string time2){
+    if(!CheckTime(time1) || !CheckTime(time2)) return false;
+
+    string num1str = time1.substr(0,2);
+    long num1 = strtol(num1str.c_str(),nullptr,10);      
+
+    string num2str = time2.substr(0,2);
+    long num2 = strtol(num2str.c_str(),nullptr,10);      
+    if(num1 < num2) return true;
+    else{
+        if(num1 > num2) return false;
+        // the rest of this else is if hours are equal
+        // so now we check for minutes
+        num1str = time1.substr(3,2);
+        num1 = strtol(num1str.c_str(),nullptr,10);      
+
+        num2str = time2.substr(3,2);
+        num2 = strtol(num2str.c_str(),nullptr,10);      
+        if(num1 < num2) return true;
+    }
+    return false;
+}
+
+bool CompareEvents(Event_t event1, Event_t event2){
+    if(event1.Action.empty() || event2.Action.empty()) return false;
+
+    if( event1.Action    == event2.Action &&
+        event1.Title     == event2.Title &&
+        event1.Time      == event2.Time &&
+        event1.Date      == event2.Date &&
+        event1.Location  == event2.Location
+       ){
+           return true;
+       }else{
+           return false;
+       }
+}
+
+multimap<string,Event_t>::iterator SearchEventByTitle(string title){
+    for(auto iter = CalendarFilter.begin(); iter != CalendarFilter.end(); ++iter){
+        if(iter->second.Title == title) return iter;
+    }
+    return CalendarFilter.end();
 }
