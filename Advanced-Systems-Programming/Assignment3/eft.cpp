a3f4dcb2f0cb4db026a0a988d43b6d5a8dadf3ac
Gian <gdutk007@gmail.com>
2021-02-18 20:30:51 -0500

    Have program working but not sure if this is the right solution. Might try again to modify this and add semaphores.

--- Advanced-Systems-Programming/Assignment3/eft.cpp
+++ Advanced-Systems-Programming/Assignment3/eft.cpp
@@ -22,6 +22,8 @@ typedef struct{
     char **argv;
 }args_t;
 
+pthread_mutex_t mLock;
+
 vector<Account_t> Accounts;
 vector<Transfer_t> Transfers;
 
@@ -29,9 +31,25 @@ static int MaxThreadNumber = 0;
 
 static void * ParseInput(void * arg);
 
+static void * SendMoney(void * arg);
+
 bool parseAccount(string accountstr);
 
 bool parseTransfer(string Transfer);
+
+void printAccountsAndTransfers(){
+    cout << "size: " << Transfers.size() << "\n"; 
+    cout << "Accounts: \n";
+    for(auto account : Accounts){
+        cout << "Id: " << account.AccountId << " Balance: " << account.Money
+         << "\n";
+    }
+    cout << "\nTransfers: \n";
+    for(auto eft : Transfers){
+        cout << "From: " << eft.From << " To: " << eft.To << " Amount: " << eft.amount
+         << "\n";
+    }
+}
 
 int main(int argc, char *argv[]){
 
@@ -72,13 +90,61 @@ static void * ParseInput(void * arg){
                 }
             }
     }
+    printAccountsAndTransfers();
+    vector<pthread_t> threads;
+    threads.resize(MaxThreadNumber);
+    for(int i = 0; i < threads.size(); ++i){
+        int * arg = new int();
+        *arg = i;
+        int ret = pthread_create(&threads[i],nullptr, SendMoney,
+                                                     (void*)arg);
+        if(ret){
+            cout << "Error creating threads, exiting. \n";
+            exit(0);
+        }
+    }
     pthread_exit(nullptr);
 }
 
 bool parseAccount(string accountstr){
-    return false;   
+    if(accountstr.empty() || accountstr.find("Transfer ") != string::npos)
+     return false;
+    istringstream is(accountstr);
+    string part;
+    Account_t Account;
+    getline(is, part, ' ');
+    Account.AccountId = strtol(part.c_str(),nullptr,10);
+    getline(is, part, ' ');
+    Account.Money = strtol(part.c_str(),nullptr,10);
+    Accounts.push_back(Account);
+    return true;   
 }
 
 bool parseTransfer(string Transfer){
-    return false;
+    if(Transfer.empty())
+     return false;
+    istringstream is(Transfer);
+    string part;
+    Transfer_t eft;
+    getline(is, part, ' ');
+    eft.From = strtol(part.c_str(),nullptr,10);
+    getline(is, part, ' ');
+    eft.To = strtol(part.c_str(),nullptr,10);
+    getline(is, part, ' ');
+    eft.amount = strtol(part.c_str(),nullptr,10);
+    Transfers.push_back(eft);
+    return true;   
+}
+
+static void * SendMoney(void * arg){
+    int tid = *((int*)arg);
+    while(Transfers.size()){
+        pthread_mutex_lock(&mLock);
+        cout << "tid: " << tid << " popping" << "\n";
+        if(Transfers.size())
+            Transfers.pop_back();
+        cout << "tid: " << tid << " size: " << Transfers.size() << "\n";
+        pthread_mutex_unlock(&mLock);
+    }
+    pthread_exit(nullptr);
 }
