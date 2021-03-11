84b590e1bd02caf2b74cc6ca6753f578fb9b6033
Gian <gdutk007@gmail.com>
2021-03-10 22:49:23 -0500

    Need to fix the delete function. After that we should be done.

--- Advanced-Systems-Programming/Assignment5/locationUpdater.cpp
+++ Advanced-Systems-Programming/Assignment5/locationUpdater.cpp
@@ -6,6 +6,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -27,13 +30,25 @@ typedef struct {
     string Location;
 } Event_t ;
 
+
+constexpr unsigned int defaultLength{256};
+
+typedef struct {
+    char ValidEmail[defaultLength];
+} Shared_Valid_Email_t;
+
+
 static int BufferLength;
 
-bool Done = false;
+bool * Shared_Done;
 
 pthread_mutex_t mLock;
 
+pthread_mutex_t * m_Lock;
+
 pthread_cond_t cv;
+
+pthread_cond_t * m_cv;
 
 multimap<string, Event_t> CalendarFilter;
 
@@ -41,13 +56,21 @@ vector<string> ValidEmails;
 
 vector<Event_t> Event_Buffer;
 
+Shared_Valid_Email_t * Shared_Valid_Emails;
+
+int * Shared_Email_count;
+
+int * Shared_Buffer_Read;
+
+int * Shared_Buffer_Write;
+
 bool parseArguments(int argc, char *argv[]);
 
-static void * ParseValidEmailsThread(void * arg);
+void ParseValidEmailsThread();
 
-static void * processCalendarThread(void * arg);
+void processCalendarThread();
 
-static void * DebuggingEmailThread(void* arg);
+void DebuggingEmailThread();
 
 bool CheckEmailString(string line);
 
@@ -69,6 +92,8 @@ bool EditEvent(Event_t Event);
 
 bool DeleteEvent(Event_t Event);
 
+bool WriteToBuffer();
+
 Event_t searchEarliestEventByDate(string date);
 
 multimap<string, Event_t>::iterator searchEventByDateTitleTime(Event_t Event);
@@ -85,28 +110,71 @@ int main(int argc, char *argv[]){
         cout << "Please Enter Valid Arguments! Program Terminating\n";
         exit(0);
     }
-    // this will be thread 1
-    // pthread_t t1;
-    // int ret1;
-    // ret1 = pthread_create(&t1,nullptr,ParseValidEmailsThread,NULL);
 
-    // // this will be thread 2
-    // pthread_t t2;
-    // int ret2;
-    // ret2 = pthread_create(&t2,nullptr,processCalendarThread,NULL);
-    // pthread_exit(nullptr);
+    pthread_mutexattr_t psharedm;
+    pthread_condattr_t psharedc;
+
+    pthread_mutexattr_init(&psharedm);
+    pthread_mutexattr_setpshared(&psharedm,PTHREAD_PROCESS_SHARED);
+    
+    pthread_condattr_init(&psharedc);
+    pthread_condattr_setpshared(&psharedc,PTHREAD_PROCESS_SHARED);
+
+    // create shared mutex
+    m_Lock = (pthread_mutex_t * ) mmap(NULL, sizeof(pthread_mutex_t), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    // create shared condition variable
+    m_cv = (pthread_cond_t * ) mmap(NULL, sizeof(pthread_cond_t), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    // init mutex and cond variable
+    pthread_mutex_init(m_Lock, &psharedm);
+    pthread_cond_init(m_cv, &psharedc);
+
+    // Shared Buffer Length
+    Shared_Valid_Emails = (Shared_Valid_Email_t * )  mmap(NULL, BufferLength * sizeof(Shared_Valid_Email_t), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    // Shared buffer count
+    Shared_Email_count = (int *) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    // shared Done
+    Shared_Done = (bool *) mmap(NULL, sizeof(bool), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    // Shared buffer Read
+    Shared_Buffer_Read = (int *) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    // Shared buffer Write
+    Shared_Buffer_Write = (int *) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    *Shared_Buffer_Read = 0;
+    *Shared_Buffer_Write = 0;
+    *Shared_Done = false;
+    *Shared_Email_count = 0; // make sure we start at count zero
+
     int pid;
     int status;
-    if(!(pid = fork())){
-
+    if(  !(pid = fork())  ){
+        // calling first process
+        ParseValidEmailsThread();
+        cout << "We should never get here... \n";
     }else{
-
+        // here we fork into the second process
+        if(!(pid = fork())){
+            // calling second process
+            processCalendarThread();
+            cout << "We should never get here... \n";
+            exit(0);
+        }else{
+            // try to wait for any children while there exists at least one
+            while ((pid=waitpid(-1,&status,0))!=-1) {
+                printf("Process %d terminated\n",pid);
+            }
+            return 0;
+        }
     }
-
-    // try to wait for any children while there exists at least one
-    while ((pid=waitpid(-1,&status,0))!=-1) {
-        printf("Process %d terminated\n",pid);
-    }
+    cout << "For some reason we didn't wait until all the processes returned....\n";
     return 0;
 }
 
@@ -126,15 +194,24 @@ bool parseArguments(int argc, char *argv[]){
     return true;
 }
 
-void * ParseValidEmailsThread(void * arg){
+void ParseValidEmailsThread(){
     for (string line; getline(cin, line);){
-        pthread_mutex_lock(&mLock);
+        pthread_mutex_lock(m_Lock);
         if(strlen(line.c_str()) > 9){
             size_t pos = line.find("Subject: ");
             if(pos != string::npos){
                 string email = line.substr(pos+strlen("Subject: "));
                 if(CheckEmailString(email)){
-                    ValidEmails.push_back(email);
+                    //cout << "From the ParseProcess the valid email is : " << email << "\n";
+                    //sprintf(Shared_Valid_Emails[*Shared_Buffer_Write].ValidEmail, "%s", email.c_str());
+                    for(int i = 0 ; i < email.size(); ++i){
+                        Shared_Valid_Emails[*Shared_Buffer_Write].ValidEmail[i] = email[i];
+                    }
+                    *Shared_Buffer_Write = (*Shared_Buffer_Write+1)%BufferLength;
+                    *Shared_Email_count += 1;
+                    // cout << "ParseProcess Read Pointer: " << *Shared_Buffer_Read << " Shared Buffer Write: " << *Shared_Buffer_Write << 
+                    //   " Email: " << email <<"\n";
+                    //cout << "Count: " << *Shared_Email_count << " Write Count:  " << *Shared_Buffer_Write << "\n";
                     /* When multi-threading this application
                        we will also push emails into a buffer
                        up to the buffer limit. Then we will 
@@ -145,19 +222,21 @@ void * ParseValidEmailsThread(void * arg){
                 }
             }
         }
-        pthread_mutex_unlock(&mLock);
+        
+        pthread_mutex_unlock(m_Lock);
         /* After unlocking, signal cond if we have reached the required length */
-        while(ValidEmails.size() >= BufferLength)
-            pthread_cond_signal(&cv);
+        //cout << "write Buff values: " << *Shared_Buffer_Write << "\n";
+        while( *Shared_Email_count >= BufferLength)
+            pthread_cond_signal(m_cv);
 
     }
-    Done = true;
+    *Shared_Done = true;
     /* signal again in case we are 
        still blocking in the other
        thread and we have stuff
        left to process */
-    pthread_cond_signal(&cv);
-    pthread_exit(nullptr);
+    pthread_cond_signal(m_cv);
+    exit(0);
 }
 
 bool CheckEmailString(string line){
@@ -241,11 +320,11 @@ bool CheckLocation(string location){
 }
 
 bool processCalendar(string email){
+    //cout << "Processing Email: " << email << " \n";
     if(email.empty()) return false;
     istringstream is(email);
     string part;
     Event_t Event;
-
     getline(is, part, ',');
     Event.Action = part;
     getline(is, part, ',');
@@ -256,7 +335,6 @@ bool processCalendar(string email){
     Event.Time = part;
     getline(is, part, ',');
     Event.Location = part;
-    
     if( Event.Action == "C"){
         CreateEvent(Event);   
     }else if(Event.Action == "X"){
@@ -299,15 +377,17 @@ bool EditEvent(Event_t Event){
 }
 
 bool DeleteEvent(Event_t Event){
+    //cout << "Delete Event: " << Event.Date << "," << Event.Time << "," << Event.Location << endl;
     auto iter = searchEventByDateTitleTime(Event);
     Event_t earliestEvent = searchEarliestEventByDate(Event.Date);
     if(!earliestEvent.Date.empty() && CompareEvents(earliestEvent,iter->second)){
         CalendarFilter.erase(iter);
-        earliestEvent = searchEarliestEventByDate(Event.Date);
-        if(!earliestEvent.Date.empty()){
-            cout << earliestEvent.Date << "," << earliestEvent.Time << "," << earliestEvent.Location << endl;  
+        cout << "Hello\n";
+        auto iter = CalendarFilter.find(earliestEvent.Date);
+        if(iter != CalendarFilter.end()){
+            cout << iter->second.Date << "," << iter->second.Time << "," << iter->second.Location << endl;  
         }
-        if(earliestEvent.Date.empty()){
+        if(iter == CalendarFilter.end()){
             cout << Event.Date <<",--:--,NA\n";
         }
     }
@@ -359,7 +439,6 @@ bool CompareTime(string time1, string time2){
 
 bool CompareEvents(Event_t event1, Event_t event2){
     if(event1.Action.empty() || event2.Action.empty()) return false;
-
     if( event1.Action    == event2.Action &&
         event1.Title     == event2.Title &&
         event1.Time      == event2.Time &&
@@ -391,38 +470,50 @@ multimap<string,Event_t>::iterator searchEventByDateTitleTime(Event_t Event){
 }
 
 
-void * processCalendarThread(void * arg){
-    while(true && !Done){
+void processCalendarThread(){
+    while(true && !*Shared_Done){
         /* Lock mutex */
-        pthread_mutex_lock(&mLock);
-        
-        while(!ValidEmails.size() && !Done )
-            pthread_cond_wait(&cv,&mLock);
+        pthread_mutex_lock(m_Lock);
+        while( *Shared_Email_count <= 0 && !*Shared_Done ){
+            pthread_cond_wait(m_cv,m_Lock);
+        }
         /* Process the emails and then erase them.
            Ideally this should have been a dequeue
            but I got lazy */
-        while(ValidEmails.size()){
-            processCalendar(ValidEmails.front());
-            ValidEmails.erase(ValidEmails.begin());
+        while( *Shared_Email_count > 0){
+            *Shared_Email_count -= 1;
+            //printf("Trying string : %s \n",&Shared_Valid_Emails[*Shared_Buffer_Read].ValidEmail[0]);
+            string email(Shared_Valid_Emails[*Shared_Buffer_Read].ValidEmail);
+            //cout << "Email: " << email << "\n";
+            //cout << "ProcessCalProcess Read Pointer: " << *Shared_Buffer_Read << " Shared Buffer Write: " << *Shared_Buffer_Write << "\n";
+            for(int i = 0; i < defaultLength; ++i){
+                Shared_Valid_Emails[*Shared_Buffer_Read].ValidEmail[i] = 0x00;
+            }
+            
+            if(processCalendar(email)){
+                //cout << "Returned good from: " << email << "\n";
+            }
+            else{cout << "For some reason the shared mem is empty\n";}
+            *Shared_Buffer_Read = (*Shared_Buffer_Read+1)%BufferLength;
         }
-        pthread_mutex_unlock(&mLock);
+        pthread_mutex_unlock(m_Lock);
     }
-    pthread_exit(nullptr);
+    exit(0);
 }
 
-void * DebuggingEmailThread(void * arg){
-    while(true && !Done){
-        pthread_mutex_lock(&mLock);
-        while(!ValidEmails.size() && !Done ){
+void DebuggingEmailThread(){
+    while(true && !*Shared_Done){
+        pthread_mutex_lock(m_Lock);
+        while(!ValidEmails.size() && !*Shared_Done ){
             cout << "Cond_wait Blocking \n";
-            pthread_cond_wait(&cv,&mLock);
+            pthread_cond_wait(m_cv,m_Lock);
         }
 
         while(ValidEmails.size()){
             cout << "The Valid Email is: " << ValidEmails.front() << endl;
             ValidEmails.erase(ValidEmails.begin());
         }
-        pthread_mutex_unlock(&mLock);
+        pthread_mutex_unlock(m_Lock);
     }
-    pthread_exit(nullptr);
+    exit(0);
 }
