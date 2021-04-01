ec85da3a8a3a6829fa74bd4678daf6073ac62e3c
Gian <gdutk007@gmail.com>
2021-03-31 21:17:15 -0400

    Added the struct and semaphore. Also added the dynamic creation of major numbers for our character device driver.

--- Advanced-Systems-Programming/Assignment5/Assignment5_resources/char_driver.c
+++ Advanced-Systems-Programming/Assignment5/Assignment5_resources/char_driver.c
@@ -16,9 +16,24 @@
 
 #include <linux/uaccess.h>	/* copy_*_user */
 
+#include <linux/semaphore.h>
+
 #define MYDEV_NAME "mycdrv"
 
 #define ramdisk_size (size_t) (16 * PAGE_SIZE) // ramdisk size 
+
+#ifndef DEFAULT_DRIVERS
+#define DEFAULT_DRIVERS 3  // default number of drivers 
+#endif
+
+struct ASP_mycdrv {
+	struct cdev dev;
+	char *ramdisk;
+	struct semaphore sem;
+	int devNo;
+};
+
+static struct ASP_mycdrv * my_ASP_mycdrv;
 
 //NUM_DEVICES defaults to 3 unless specified during insmod
 int NUM_DEVICES = 3;
@@ -26,30 +41,118 @@ int NUM_DEVICES = 3;
 #define CDRV_IOC_MAGIC 'Z'
 #define ASP_CLEAR_BUF _IOW(CDRV_IOC_MAGIC, 1, int)
 
-struct ASP_mycdrv {
-	struct cdev dev;
-	char *ramdisk;
-	struct semaphore sem;
-	int devNo;
-	// any other field you may want to add
-};
+static int major = 0, minor = 0;
 
-
-static int my_init(void){
-
-    pr_info("\nSucceeded in registering character device %s\n", MYDEV_NAME);
-    return 0;
+static int mycdrv_open(struct inode *inode, struct file *file)
+{
+	pr_info(" OPENING device: %s:\n\n", MYDEV_NAME);
+	return 0;
 }
 
-static void my_exit(void){
+static int mycdrv_release(struct inode *inode, struct file *file)
+{
+	pr_info(" CLOSING device: %s:\n\n", MYDEV_NAME);
+	return 0;
+}
+
+static ssize_t
+mycdrv_read(struct file *file, char __user * buf, size_t lbuf, loff_t * ppos)
+{
+	int nbytes = 0;
+	// if ((lbuf + *ppos) > ramdisk_size) {
+	// 	pr_info("trying to read past end of device,"
+	// 		"aborting because this is just a stub!\n");
+	// 	return 0;
+	// }
+	// nbytes = lbuf - copy_to_user(buf, ramdisk + *ppos, lbuf);
+	// *ppos += nbytes;
+	// pr_info("\n READING function, nbytes=%d, pos=%d\n", nbytes, (int)*ppos);
+	return nbytes;
+}
+
+static ssize_t
+mycdrv_write(struct file *file, const char __user * buf, size_t lbuf,
+	     loff_t * ppos)
+{
+	int nbytes = 0;
+	// if ((lbuf + *ppos) > ramdisk_size) {
+	// 	pr_info("trying to read past end of device,"
+	// 		"aborting because this is just a stub!\n");
+	// 	return 0;
+	// }
+	// nbytes = lbuf - copy_from_user(ramdisk + *ppos, buf, lbuf);
+	// *ppos += nbytes;
+	// pr_info("\n WRITING function, nbytes=%d, pos=%d\n", nbytes, (int)*ppos);
+	return nbytes;
+}
+
+
+static const struct file_operations character_driver_fops = {
+	.owner = THIS_MODULE,
+	.read = mycdrv_read,
+	.write = mycdrv_write,
+	.open = mycdrv_open,
+	.release = mycdrv_release,
+};
+
+/*
+ * Set up the char_dev structure for this device.
+ */
+static void setup_cdev(struct ASP_mycdrv *dev, int index)
+{
+	int err, devno = MKDEV(major, minor + index);
+    
+	cdev_init(&dev->dev, &character_driver_fops);
+	dev->dev.owner = THIS_MODULE;
+	err = cdev_add (&dev->dev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk(KERN_NOTICE "Error %d adding character device %d", err, index);
+}
+static int init_driver(void){
+
+	int result, i;
+	dev_t dev = 0;
+	result = alloc_chrdev_region(&dev, minor, NUM_DEVICES,"scull");
+	major = MAJOR(dev);
+	if (result < 0) {
+		printk(KERN_WARNING 
+			"DRIVER: can't allocate a major driver, result: %d\n", result);
+		return result;
+	}
+
+	// allocating a dynamic number of devices here
+	my_ASP_mycdrv = kmalloc(NUM_DEVICES * sizeof(struct ASP_mycdrv), GFP_KERNEL);
+	if (!my_ASP_mycdrv) {
+		result = -ENOMEM;
+		goto fail;  /* Make this more graceful */
+	}
+	memset(my_ASP_mycdrv, 0x00, NUM_DEVICES * sizeof(struct ASP_mycdrv));
+
+
+	// now we need to initialize them
+	for(i = 0; i < NUM_DEVICES; ++i){
+		my_ASP_mycdrv[i].devNo = i;
+		my_ASP_mycdrv[i].ramdisk = kmalloc(ramdisk_size, GFP_KERNEL);
+		sema_init(&my_ASP_mycdrv[i].sem,1);
+		setup_cdev(&my_ASP_mycdrv[i],i);
+	}
+    pr_info("\nSucceeded in registering character device %s\n", MYDEV_NAME);
+    return 0;
+  fail:
+	//scull_cleanup_module();
+	return result;
+}
+
+static void exit_driver(void){
 
     pr_info("\ndevice unregistered\n");
 
 }
 
 
-module_init(my_init);
-module_exit(my_exit);
+module_init(init_driver);
+module_exit(exit_driver);
 
 MODULE_AUTHOR("user");
 MODULE_LICENSE("GPL v2");
--- Advanced-Systems-Programming/Assignment5/LDD_sol/SOLUTIONS/s_02/lab1_chrdrv.c
+++ Advanced-Systems-Programming/Assignment5/LDD_sol/SOLUTIONS/s_02/lab1_chrdrv.c
@@ -34,7 +34,7 @@ static char *ramdisk;
 
 static dev_t first;
 static unsigned int count = 1;
-static int my_major = 700, my_minor = 0;
+static int minor = 0;
 static struct cdev *my_cdev;
 
 static int mycdrv_open(struct inode *inode, struct file *file)
@@ -88,11 +88,17 @@ static const struct file_operations mycdrv_fops = {
 	.release = mycdrv_release,
 };
 
-static int __init my_init(void)
+static int __init init_driver(void)
 {
-	ramdisk = kmalloc(ramdisk_size, GFP_KERNEL);
+	int result, i;
+	dev_t dev = 0;
+	result = alloc_chrdev_region(&dev, minor, scull_nr_devs,"scull");
+	scull_major = MAJOR(dev);
+
+	// create dev which holds major and minor numbers
 	first = MKDEV(my_major, my_minor);
-	register_chrdev_region(first, count, MYDEV_NAME);
+
+
 	my_cdev = cdev_alloc();
 	cdev_init(my_cdev, &mycdrv_fops);
 	cdev_add(my_cdev, first, count);
@@ -100,7 +106,7 @@ static int __init my_init(void)
 	return 0;
 }
 
-static void __exit my_exit(void)
+static void __exit exit_driver(void)
 {
 	cdev_del(my_cdev);
 	unregister_chrdev_region(first, count);
@@ -108,8 +114,8 @@ static void __exit my_exit(void)
 	kfree(ramdisk);
 }
 
-module_init(my_init);
-module_exit(my_exit);
+module_init(init_driver);
+module_exit(exit_driver);
 
 MODULE_AUTHOR("Jerry Cooperstein");
 MODULE_LICENSE("GPL v2");
