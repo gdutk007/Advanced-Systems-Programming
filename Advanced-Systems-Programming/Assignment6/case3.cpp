8bb88e7d4893391e688088cddd97a52db12eeafd
Gian <gdutk007@gmail.com>
2021-04-11 17:08:44 -0400

    Finished case 3.

--- Advanced-Systems-Programming/Assignment6/case3.cpp
+++ Advanced-Systems-Programming/Assignment6/case3.cpp
@@ -22,7 +22,19 @@ using namespace std;
 
 
     /*           Case 3
-        
+        For this third case two threads will try and switch their modes. 
+
+        While trying to switch modes there can be a case where both 
+        threads are left waiting for locks 1 and 2 to be released. This 
+        will never happen under the following scenerio. 
+
+           - The first thread will open the driver and switch its mode to mode 2 and sleep. 
+           - The second thread will then open the driver and lock semaphore 2.
+           - Then thread 2 will try to switch to mode 1, but will block trying to lock 
+           sempahore 2 because open() already locked it. 
+           - Now because we block trying to switch back to mode 1, we will be back in thread
+           1. Thread 1 will try to switch the mode to mode 1 and will fail because thread 2 
+           has locked sempahores 1 and 2 when trying to switch its mode. 
     */
 
 static void * thread1(void * arg){
@@ -34,7 +46,12 @@ static void * thread1(void * arg){
     fd1 = open("/dev/a5", O_RDWR);
     io = ioctl(fd1,E2_IOCMODE2,0);            
     sleep(4);
-    fd2 = open("/dev/a5", O_RDWR); 
+    /*
+       since semaphore 1 and 2 are locked now 
+       we will block trying to lock semaphore 1. 
+       This will cause a deadlock. 
+    */
+    io = ioctl(fd1,E2_IOCMODE1,0);            
     pthread_exit(nullptr);
 }
 
@@ -45,7 +62,11 @@ static void * thread2(void * arg){
     io = 0;
     sleep(1); // we want to make sure this doesn't run first
     fd1 = open("/dev/a5", O_RDWR);
-    io = ioctl(fd1,E2_IOCMODE1,0); // we should block switching to mode 1 
+    /*
+       Since semaphore 2 is still locked we will block trying to 
+       switch back to mode 1.
+    */
+    io = ioctl(fd1,E2_IOCMODE1,0);  
     pthread_exit(nullptr);
 }
 
